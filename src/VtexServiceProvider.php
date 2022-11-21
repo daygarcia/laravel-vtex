65c216eea21afcd04d0fb784c0545d2481b8ea8f
DayGarcia <dayannegarcia@foreverliss.com.br>
2022-11-21 17:43:52 -0300

    comment request publisher

--- src/VtexServiceProvider.php
+++ src/VtexServiceProvider.php
@@ -31,12 +31,12 @@ class VtexServiceProvider extends ServiceProvider
             __DIR__ . '/config/vtex.php' => config_path('vtex.php'),
         ]);
         // check if the App/Http/Requests/LaravelVtex directory exists
-        if (!file_exists(app_path('Http/Requests/LaravelVtex/'))) {
+        /* if (!file_exists(app_path('Http/Requests/LaravelVtex/'))) {
             mkdir(app_path('Http/Requests/LaravelVtex/'), 0755, true);
         }
         $this->publishes([
             __DIR__ . '/app/Http/Requests/' => app_path('Http/Requests/LaravelVtex/'),
-        ]);
+        ]); */
 
         $this->client_id = config('vtex.app_key');
         $this->client_secret = config('vtex.app_token');
