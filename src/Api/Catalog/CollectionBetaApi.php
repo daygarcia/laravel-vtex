44993f9ecdafde069ec13a75ca1d4808781de040
DayGarcia <dayannegarcia@foreverliss.com.br>
2022-11-22 09:41:10 -0300

    temporary remove unnecessary params

--- src/Api/Catalog/CollectionBetaApi.php
+++ src/Api/Catalog/CollectionBetaApi.php
@@ -4,6 +4,7 @@ namespace LaravelVtex\Api\Catalog;
 
 use LaravelVtex\Api;
 use LaravelVtex\Configuration;
+use Models\Catalog\GetProductsFromACollection;
 
 class CollectionBetaApi extends Api
 {
@@ -13,9 +14,9 @@ class CollectionBetaApi extends Api
         parent::__construct();
     }
 
-    public function getProductsFromACollection(string $id, string $page, string $page_size, string $filter, bool $active, bool $visible, int $category_id, int $brand_id, int $supplier_id, int $sales_channel_id, string $release_from, string $release_to, string $specification_product, int $specification_field_id)
+    public function getProductsFromACollection(string $id)
     {
-        $url = "catalog/pvt/collection/{$id}/products?page={$page}&pageSize={$page_size}";
+        $url = "catalog/pvt/collection/{$id}/products";
         return $this->get($this->configuration, $url);
     }
 }
